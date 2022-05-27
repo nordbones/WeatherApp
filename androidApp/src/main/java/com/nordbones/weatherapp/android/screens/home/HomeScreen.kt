6a19ee46dbcc3f735a4e1bbce35bf3564bb5a0f3
Nikita <mikita.seviarynchyk@softensity.com>
2022-05-27 17:07:24 +0300

    added screens home and settings to android app

--- androidApp/src/main/java/com/nordbones/weatherapp/android/screens/home/HomeScreen.kt
+++ androidApp/src/main/java/com/nordbones/weatherapp/android/screens/home/HomeScreen.kt
@@ -1,2 +1,43 @@
 package com.nordbones.weatherapp.android.screens.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.nordbones.weatherapp.viewmodel.ViewState
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
+    val weatherState by viewModel.weatherState.collectAsState(initial = ViewState())
+
+
+    Scaffold() { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = paddingValues.calculateTopPadding(),
+                    bottom = paddingValues.calculateBottomPadding()
+                )
+        ) {
+            Button(onClick = viewModel::getWeather) {
+                Text(text = "Get Weather")
+            }
+
+            when {
+                weatherState.isLoading -> CircularProgressIndicator()
+                weatherState.data != null -> weatherState.data?.let { weather ->
+                    Text(text = weather.toString())
+                }
+                weatherState.error != null -> Text("Error: ${weatherState.error?.message}")
+            }
+        }
+    }
+}
--- androidApp/src/main/java/com/nordbones/weatherapp/android/screens/settings/SettingsScreen.kt
+++ androidApp/src/main/java/com/nordbones/weatherapp/android/screens/settings/SettingsScreen.kt
@@ -1,2 +1,56 @@
 package com.nordbones.weatherapp.android.screens.settings
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.nordbones.weatherapp.android.utils.TextFieldState
+import com.nordbones.weatherapp.viewmodel.ViewState
+
+
+@Composable
+fun SettingsScreen(viewModel: SettingsViewModel) {
+    val locationFieldState by viewModel.locationFieldState.collectAsState(TextFieldState.empty)
+    val locationState by viewModel.locationState.collectAsState(initial = ViewState())
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        TextField(
+            value = locationFieldState.text,
+            onValueChange = viewModel::onLocationChange,
+            placeholder = { Text("Location") },
+            isError = locationFieldState.error,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(onClick = viewModel::searchLocation) {
+            Text(text = "Search")
+        }
+
+        when {
+            locationState.isLoading -> CircularProgressIndicator()
+            locationState.data != null -> locationState.data?.let { locations ->
+                LazyColumn {
+                    locations.list.forEach { location ->
+                        item {
+                            Text(
+                                text = "${location.name}, ${location.region}, ${location.country}",
+                                modifier = Modifier.clickable {
+                                    viewModel.saveLocation(location)
+                                })
+                        }
+                    }
+                }
+            }
+            locationState.error != null -> Text("Error: ${locationState.error?.message}")
+        }
+    }
+}
