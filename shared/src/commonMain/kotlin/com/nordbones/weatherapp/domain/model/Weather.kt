96e7a90f638c2cbaff6e291ccfebb55478ad7fb6
Nikita <mikita.seviarynchyk@softensity.com>
2022-06-02 15:41:50 +0300

    [Shared] added to Weather.kt companion method fromDTO extracted this logic from GetWeatherUseCaseImpl.kt

--- shared/src/commonMain/kotlin/com/nordbones/weatherapp/domain/model/Weather.kt
+++ shared/src/commonMain/kotlin/com/nordbones/weatherapp/domain/model/Weather.kt
@@ -1,12 +1,15 @@
 package com.nordbones.weatherapp.domain.model
 
+import com.nordbones.weatherapp.data.dto.LocationDTO
+import com.nordbones.weatherapp.data.dto.WeatherDTO
+
 
 data class Weather(
     val description: String,
     val windSpeedInMpH: Double,
     val windDirectionCompass: String,
     val windDirection: Double,
-    val temp: Double,
+    val temp: Int,
     val airPressure: Double,
     val humidityInPercents: Float,
     val visibilityInMiles: Double,
@@ -16,4 +19,37 @@ data class Weather(
     val country: String,
     val longitude: Double,
     val latitude: Double
-)
+) {
+
+    companion object {
+        fun fromDTO(weatherDTO: WeatherDTO, locationDTO: LocationDTO): Weather = Weather(
+            description = weatherDTO.description,
+            windSpeedInMpH = weatherDTO.windSpeedInMpH,
+            windDirection = weatherDTO.windDirection,
+            windDirectionCompass = convertDegreesToCompassDirection(weatherDTO.windDirection),
+            temp = weatherDTO.temp.toInt(),
+            airPressure = weatherDTO.airPressure,
+            humidityInPercents = weatherDTO.humidityInPercents,
+            visibilityInMiles = weatherDTO.visibilityInMiles,
+            apparentTemp = weatherDTO.apparentTemp,
+            name = locationDTO.name,
+            region = locationDTO.region,
+            country = locationDTO.country,
+            longitude = locationDTO.longitude,
+            latitude = locationDTO.latitude
+        )
+
+        private fun convertDegreesToCompassDirection(degrees: Double): String {
+            val sector = 360 / 4
+            val halfOfSector: Double = (sector / 2).toDouble()
+            return when (degrees) {
+                in (0 - halfOfSector)..(0 + halfOfSector) -> "North"
+                in (sector - halfOfSector)..(sector + halfOfSector) -> "East"
+                in (sector * 2 - halfOfSector)..(sector * 2 + halfOfSector) -> "South"
+                in (sector * 3 - halfOfSector)..(sector * 3 + halfOfSector) -> "West"
+                else -> "None"
+            }
+        }
+    }
+}
+
--- shared/src/commonMain/kotlin/com/nordbones/weatherapp/domain/usecase/GetWeatherUseCaseImpl.kt
+++ shared/src/commonMain/kotlin/com/nordbones/weatherapp/domain/usecase/GetWeatherUseCaseImpl.kt
@@ -5,6 +5,7 @@ import com.nordbones.weatherapp.domain.repository.WeatherRepository
 import com.nordbones.weatherapp.utils.ResultWrapper
 import com.nordbones.weatherapp.utils.onSuccess
 import com.nordbones.weatherapp.domain.model.Weather
+import com.nordbones.weatherapp.utils.onError
 import kotlinx.coroutines.flow.*
 
 class GetWeatherUseCaseImpl constructor(
@@ -13,42 +14,19 @@ class GetWeatherUseCaseImpl constructor(
 ) :
     GetWeatherUseCase {
     override suspend fun invoke(): Flow<ResultWrapper<Weather>> {
-        val location = locationRepository.getLocation()
-        return weatherRepository.getWeather(
-            longitude = location.longitude,
-            latitude = location.latitude
+        val locationDTO = locationRepository.getLocation()
+        return weatherRepository.getWeatherFromLocalStore(
+            longitude = locationDTO.longitude,
+            latitude = locationDTO.latitude
         ).map { result ->
-            result.onSuccess { weatherDTO ->
-                Weather(
-                    description = weatherDTO.description,
-                    windSpeedInMpH = weatherDTO.windSpeedInMpH,
-                    windDirection = weatherDTO.windDirection,
-                    windDirectionCompass = convertDegreesToCompassDirection(weatherDTO.windDirection),
-                    temp = weatherDTO.windDirection,
-                    airPressure = weatherDTO.airPressure,
-                    humidityInPercents = weatherDTO.humidityInPercents,
-                    visibilityInMiles = weatherDTO.visibilityInMiles,
-                    apparentTemp = weatherDTO.apparentTemp,
-                    name = location.name,
-                    region = location.region,
-                    country = location.country,
-                    longitude = location.longitude,
-                    latitude = location.latitude
-                )
-            }
-        }
-
-    }
-
-    private fun convertDegreesToCompassDirection(degrees: Double): String {
-        val sector = 360 / 4
-        val halfOfSector: Double = (sector / 2).toDouble()
-        return when (degrees) {
-            in (0 - halfOfSector)..(0 + halfOfSector) -> "North"
-            in (sector - halfOfSector)..(sector + halfOfSector) -> "East"
-            in (sector * 2 - halfOfSector)..(sector * 2 + halfOfSector) -> "South"
-            in (sector * 3 - halfOfSector)..(sector * 3 + halfOfSector) -> "West"
-            else -> "None"
+            result
+                .onSuccess { weatherDTO ->
+                    weatherRepository.saveWeatherToLocalStore(weatherDTO)
+                    Weather.fromDTO(weatherDTO = weatherDTO, locationDTO = locationDTO)
+                }.onError {
+                    val weatherDTO = weatherRepository.getWeatherFromLocalStore()
+                    Weather.fromDTO(weatherDTO = weatherDTO, locationDTO = locationDTO)
+                }
         }
     }
 
