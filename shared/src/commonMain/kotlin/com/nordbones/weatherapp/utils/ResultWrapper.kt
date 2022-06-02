cdaa1f777b523e8089cc47a7da3ca30ddaee812d
Nikita <mikita.seviarynchyk@softensity.com>
2022-06-02 15:25:52 +0300

    [Shared] Added to ResultWrapper.Error new filed defaultData Added to ResultWrapper.kt extension onError Added to ResultWrapper.kt extensions inline modifier

--- shared/src/commonMain/kotlin/com/nordbones/weatherapp/utils/ResultWrapper.kt
+++ shared/src/commonMain/kotlin/com/nordbones/weatherapp/utils/ResultWrapper.kt
@@ -7,15 +7,25 @@ import kotlinx.coroutines.flow.onStart
 
 sealed interface ResultWrapper<out T> {
     data class Success<T>(val data: T) : ResultWrapper<T>
-    data class Error(val exception: Throwable? = null) : ResultWrapper<Nothing>
+    data class Error<T>(val exception: Throwable? = null, val defaultData: T? = null) :
+        ResultWrapper<T>
+
     object Loading : ResultWrapper<Nothing>
 }
 
-fun <T, R> ResultWrapper<T>.onSuccess(map: (T) -> R): ResultWrapper<R> {
+inline fun <T, R> ResultWrapper<T>.onSuccess(map: (T) -> R): ResultWrapper<R> {
     return when (this) {
         is ResultWrapper.Error -> ResultWrapper.Error(this.exception)
         ResultWrapper.Loading -> ResultWrapper.Loading
         is ResultWrapper.Success -> ResultWrapper.Success(map(this.data))
+    }
+}
+
+inline fun <T, R> ResultWrapper<T>.onError(map: () -> R): ResultWrapper<R> {
+    return when (this) {
+        is ResultWrapper.Error -> ResultWrapper.Error(this.exception, map())
+        ResultWrapper.Loading -> ResultWrapper.Loading
+        is ResultWrapper.Success -> ResultWrapper.Success(map())
     }
 }
 
