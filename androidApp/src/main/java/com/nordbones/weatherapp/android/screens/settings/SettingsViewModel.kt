6ec7df95d3bedf5472d90cec5b347aa4ed50d1ab
Nikita <mikita.seviarynchyk@softensity.com>
2022-06-06 17:44:35 +0300

    [Android] Implemented SettingsViewModel.kt

--- androidApp/src/main/java/com/nordbones/weatherapp/android/screens/settings/SettingsViewModel.kt
+++ androidApp/src/main/java/com/nordbones/weatherapp/android/screens/settings/SettingsViewModel.kt
@@ -5,13 +5,13 @@ import androidx.lifecycle.viewModelScope
 import com.nordbones.weatherapp.android.utils.TextFieldState
 import com.nordbones.weatherapp.domain.model.Location
 import com.nordbones.weatherapp.domain.model.Locations
+import com.nordbones.weatherapp.utils.CommonFlow
+import com.nordbones.weatherapp.utils.EMPTY
 import com.nordbones.weatherapp.viewmodel.LocationSharedViewModel
 import com.nordbones.weatherapp.viewmodel.ViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -26,29 +26,54 @@ class SettingsViewModel @Inject constructor(private val locationSharedViewModel:
     private val mLocationState: MutableSharedFlow<ViewState<Locations>> = MutableSharedFlow()
     val locationState: SharedFlow<ViewState<Locations>> = mLocationState
 
+    init {
+        getLocation()
+    }
+
     fun onLocationChange(value: String) {
         viewModelScope.launch {
+            val isNotValid = value.length < 5
             mLocationFieldState.emit(
-                value = TextFieldState(text = value, error = value.length < 3)
+                value = TextFieldState(text = value, error = isNotValid)
             )
+            if (!isNotValid) {
+                locationSharedViewModel.searchLocation(value)
+                    .collectInScope(viewModelScope) {
+                        mLocationState.emit(it)
+                    }
+            }
         }
     }
 
-    fun searchLocation() {
-        if (!mLocationFieldState.value.error) {
-            locationSharedViewModel.searchLocation(location = mLocationFieldState.value.text)
-                .collectCommon { state ->
-                    viewModelScope.launch {
-                        mLocationState.emit(state)
-                    }
-                }
+    fun onLocationClear() {
+        viewModelScope.launch {
+            mLocationFieldState.emit(
+                value = TextFieldState(text = String.EMPTY, error = false)
+            )
         }
     }
 
     fun saveLocation(location: Location) {
         viewModelScope.launch {
             locationSharedViewModel.saveLocation(location = location)
+            mLocationState.emit(ViewState())
+            mLocationFieldState.emit(TextFieldState(text = location.fullTitle, error = false))
         }
     }
 
+    private fun getLocation() {
+        viewModelScope.launch {
+            locationSharedViewModel.getLocation().apply {
+                mLocationFieldState.emit(
+                    TextFieldState(text = fullTitle, error = false)
+                )
+            }
+        }
+    }
+}
+
+fun <T> CommonFlow<T>.collectInScope(scope: CoroutineScope, callback: suspend (T) -> Unit) {
+    onEach {
+        callback(it)
+    }.launchIn(scope)
 }
