1d633f52bb345941123e57846c792ed04c68e916
Nikita <mikita.seviarynchyk@softensity.com>
2022-06-06 17:31:14 +0300

    [iOS] Implemented SettingsViewModel.swift

--- iosApp/iosApp/Screens/Settigns/SettingsViewModel.swift
+++ iosApp/iosApp/Screens/Settigns/SettingsViewModel.swift
@@ -8,20 +8,51 @@
 
 import Foundation
 import shared
+import SwiftUI
 
-class SettingsViewModel:ObservableObject{
+class SettingsViewModel:ObservableObject {
     private let sharedViewModel = appModule.locationSharedViewModel
     
     @Published var state = ViewState<Locations>(isLoading: false, data: nil, error: nil)
+    @Published var location = String.EMPTY
+    {
+        didSet {
+        if location.count > 5 {
+            searchLocation(value: location)
+        } else {
+            self.state = ViewState<Locations>(isLoading: false, data: nil, error: nil)
+        }
+        }
+    }
     
-    func searchLocation(value:String){
+    func searchLocation(value:String) {
         sharedViewModel.searchLocation(location: value).collectCommon { result in
             self.state = result!
         }
     }
     
-    func saveLocation(location:Location){
+    func onLocationClear() {
+        self.location = String.EMPTY
+    }
+    
+    func saveLocation(location:Location) {
         sharedViewModel.saveLocation(location: location) { _, _ in
         }
+        self.location = location.getFullTitle()
+        self.state = ViewState<Locations>(isLoading: false, data: nil, error: nil)
+    }
+    
+    func getLocation() {
+        sharedViewModel.getLocation { location, error in
+            if location != nil && error == nil {
+                self.location = location?.getFullTitle() ?? String.EMPTY
+            }
+        }
+    }
+}
+
+extension Location {
+    func getFullTitle() -> String {
+        return "\(name),\(region),\(country)"
     }
 }
